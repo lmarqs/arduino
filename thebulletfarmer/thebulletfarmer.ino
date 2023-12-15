c8869b2c97ff53108ee9e5d7340f4d716d749445
lmarqs <marques.analista@gmail.com>
2023-12-15 00:17:56 -0300

    Add L298PWheelChair and L298NWheelChair classes

---
+++ thebulletfarmer/thebulletfarmer.ino
@@ -0,0 +1,40 @@
+#include <Arduino.h>
+
+#include "WheelChair.h
+
+L298PWheelChair Car(25, 23, 19, 18);
+
+void setup()
+{
+  Serial.begin(9600);
+  Car.begin();
+}
+
+void loop()
+{
+  int const speed = 120;
+  Car.goFoward();
+  Car.accelerate(speed);
+  delay(1000);
+
+  Car.accelerate(0);
+  delay(1000);
+
+  Car.goBackward();
+  Car.accelerate(speed);
+  delay(1000);
+
+  Car.accelerate(0);
+  delay(1000);
+
+  Car.turnLeft();
+  Car.accelerate(speed);
+  delay(1000);
+
+  Car.accelerate(0);
+  delay(1000);
+
+  Car.turnRight();
+  Car.accelerate(speed);
+  delay(1000);
+}
