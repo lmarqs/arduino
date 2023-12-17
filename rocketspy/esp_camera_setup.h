4eac79f3bccbe56cecea253057c7118c8148371d
lmarqs <marques.analista@gmail.com>
2023-12-17 18:21:19 -0300

    Update esp_camera_setup() function to return sensor_t pointer

--- rocketspy/esp_camera_setup.h
+++ rocketspy/esp_camera_setup.h
@@ -1,24 +1,9 @@
-#include "esp_camera.h"
+#include <Arduino.h>
+#include <esp_camera.h>
 
-void esp_camera_led_setup(int pin)
-{
-#if CONFIG_LED_ILLUMINATOR_ENABLED
-  ledcSetup(LED_LEDC_CHANNEL, 5000, 8);
-  ledcAttachPin(pin, LED_LEDC_CHANNEL);
-#else
-  log_i("LED flash is disabled -> CONFIG_LED_ILLUMINATOR_ENABLED = 0");
-#endif
-}
-
-void esp_camera_setup()
+sensor_t *esp_camera_setup()
 {
   camera_config_t config;
-  // config.frame_size = FRAMESIZE_UXGA;
-  // config.pixel_format = PIXFORMAT_JPEG;
-  // config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
-  // config.fb_location = CAMERA_FB_IN_DRAM;
-  // config.jpeg_quality = 12;
-  // config.fb_count = 1;
 
   config.ledc_channel = LEDC_CHANNEL_0;
   config.ledc_timer = LEDC_TIMER_0;
@@ -38,23 +23,23 @@ void esp_camera_setup()
   config.pin_sscb_scl = SIOC_GPIO_NUM;
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
+  config.xclk_freq_hz = 16000000;
   config.pixel_format = PIXFORMAT_JPEG;
-  config.jpeg_quality = 10;
 
+  config.jpeg_quality = 12;
   config.fb_count = 1;
   config.fb_location = CAMERA_FB_IN_DRAM;
-  config.frame_size = FRAMESIZE_VGA;
+  config.frame_size = FRAMESIZE_240X240;
   config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
 
   if (psramFound())
   {
+    config.jpeg_quality = 10;
     config.fb_count = 2;
     config.fb_location = CAMERA_FB_IN_PSRAM;
     config.frame_size = FRAMESIZE_UXGA;
     config.grab_mode = CAMERA_GRAB_LATEST;
   }
-
 
 #if defined(CAMERA_MODEL_ESP_EYE)
   pinMode(13, INPUT_PULLUP);
@@ -66,34 +51,31 @@ void esp_camera_setup()
   if (err != ESP_OK)
   {
     Serial.printf("Camera init failed with error 0x%x\n", err);
-    return;
+    return NULL;
   }
 
-  sensor_t *s = esp_camera_sensor_get();
+  sensor_t *sensor = esp_camera_sensor_get();
 
-  if (s->id.PID == OV3660_PID)
+  if (sensor->id.PID == OV3660_PID)
   {
-    s->set_vflip(s, 1);
-    s->set_brightness(s, 1);
-    s->set_saturation(s, -2);
-  }
-
-  if (config.pixel_format == PIXFORMAT_JPEG)
-  {
-    s->set_framesize(s, FRAMESIZE_QVGA);
+    sensor->set_vflip(sensor, 1);
+    sensor->set_brightness(sensor, 1);
+    sensor->set_saturation(sensor, -2);
   }
 
 #if defined(CAMERA_MODEL_M5STACK_WIDE) || defined(CAMERA_MODEL_M5STACK_ESP32CAM)
-  s->set_vflip(s, 1);
-  s->set_hmirror(s, 1);
+  sensor->set_vflip(sensor, 1);
+  sensor->set_hmirror(sensor, 1);
 #endif
 
 #if defined(CAMERA_MODEL_ESP32S3_EYE)
-  s->set_vflip(s, 1);
+  sensor->set_vflip(sensor, 1);
 #endif
 
-// Setup LED FLash if LED pin is defined in camera_pins.h
-#if defined(LED_GPIO_NUM)
-  esp_camera_led_setup(LED_GPIO_NUM);
+#if defined(LED_GPIO_NUM) && CONFIG_LED_ILLUMINATOR_ENABLED
+  ledcSetup(LED_GPIO_NUM, 5000, 8);
+  ledcAttachPin(pin, LED_GPIO_NUM);
 #endif
+
+  return sensor;
 }
