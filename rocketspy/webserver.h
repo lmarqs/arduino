27cec0479ae0d41beadfdd14e5ae2934b07ca787
lmarqs <marques.analista@gmail.com>
2023-12-17 18:17:36 -0300

    Add RocketSpy web server classes and methods

---
+++ rocketspy/webserver.h
@@ -0,0 +1,132 @@
+#include <Arduino.h>
+#include <esp_http_server.h>
+
+class RocketSpyRequest
+{
+private:
+  httpd_req_t *req;
+
+public:
+  RocketSpyRequest(httpd_req_t *req)
+  {
+    this->req = req;
+  }
+};
+
+class RocketSpyResponse
+{
+private:
+  httpd_req_t *req;
+  esp_err_t err;
+
+public:
+  RocketSpyResponse(httpd_req_t *req)
+  {
+    this->req = req;
+    this->err = ESP_OK;
+  }
+
+  esp_err_t getErr()
+  {
+    return err;
+  }
+
+  void reset()
+  {
+    err = ESP_OK;
+  }
+
+  void fail()
+  {
+    err = ESP_FAIL;
+  }
+
+  void setStatus(const char *status)
+  {
+    if (err != ESP_OK)
+    {
+      return;
+    }
+
+    err = httpd_resp_set_status(req, status);
+  }
+
+  void setType(const char *type)
+  {
+    if (err != ESP_OK)
+    {
+      return;
+    }
+
+    err = httpd_resp_set_type(req, type);
+  }
+
+  void setHeader(const char *key, const char *value)
+  {
+    if (err != ESP_OK)
+    {
+      return;
+    }
+
+    err = httpd_resp_set_hdr(req, key, value);
+  }
+
+  void write(const char *buf, ssize_t len)
+  {
+    if (err != ESP_OK)
+    {
+      return;
+    }
+
+    err = httpd_resp_send_chunk(req, buf, len);
+  }
+
+  void send(const char *status, const char *type, const char *buf, ssize_t len)
+  {
+    this->setStatus(status);
+    this->setType(type);
+    err = httpd_resp_send(req, buf, len);
+  }
+};
+
+using RocketSpyWebServerHandler = std::function<void(RocketSpyRequest *req, RocketSpyResponse *res)>;
+
+class RocketSpyWebServer
+{
+private:
+  httpd_handle_t httpd = NULL;
+
+public:
+  void begin()
+  {
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+
+    httpd_start(&httpd, &config);
+
+    Serial.println("HTTP webserver started");
+  }
+
+  void on(const char *uri, const httpd_method_t method, const RocketSpyWebServerHandler handler)
+  {
+    httpd_uri_t config = {
+        .uri = uri,
+        .method = method,
+        .handler = [](httpd_req_t *r) -> esp_err_t
+        {
+          RocketSpyRequest req(r);
+          RocketSpyResponse res(r);
+
+          RocketSpyWebServerHandler *handler = static_cast<RocketSpyWebServerHandler *>(r->user_ctx);
+
+          (*handler)(&req, &res);
+
+          return res.getErr();
+        },
+        .user_ctx = new RocketSpyWebServerHandler(handler),
+        .is_websocket = true,
+        .handle_ws_control_frames = false,
+        .supported_subprotocol = NULL};
+
+    httpd_register_uri_handler(httpd, &config);
+  }
+};
