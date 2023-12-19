b21f5f9dd24b1d1a64afd83b7bcdf3bd50742474
lmarqs <marques.analista@gmail.com>
2023-12-19 00:04:49 -0300

    format source

--- immortanjoe/immortanjoe.ino
+++ immortanjoe/immortanjoe.ino
@@ -1,6 +1,6 @@
-#include <RoboCore_Vespa.h>
 #include <Arduino.h>
 #include <Bluepad32.h>
+#include <RoboCore_Vespa.h>
 
 VespaMotors motors;
 Controller *controller = nullptr;
@@ -12,49 +12,39 @@ const uint8_t MOTOR_MAX_SPEED = 100;
 const int32_t CONTROLLER_AXIS_DEATH_VALUE = 128;
 const int32_t CONTROLLER_AXIS_MAX_VALUE = 512;
 
-int32_t hardcapControllerAxisValue(int32_t axisValue)
-{
-  if (axisValue > CONTROLLER_AXIS_MAX_VALUE)
-  {
+int32_t hardcapControllerAxisValue(int32_t axisValue) {
+  if (axisValue > CONTROLLER_AXIS_MAX_VALUE) {
     return CONTROLLER_AXIS_MAX_VALUE;
   }
 
-  if (axisValue < -CONTROLLER_AXIS_MAX_VALUE)
-  {
+  if (axisValue < -CONTROLLER_AXIS_MAX_VALUE) {
     return -CONTROLLER_AXIS_MAX_VALUE;
   }
 
   return axisValue;
 }
 
-int32_t ignoreControllerAxisDeathZone(int32_t axisValue)
-{
-  if (axisValue < CONTROLLER_AXIS_DEATH_VALUE && axisValue > -CONTROLLER_AXIS_DEATH_VALUE)
-  {
+int32_t ignoreControllerAxisDeathZone(int32_t axisValue) {
+  if (axisValue < CONTROLLER_AXIS_DEATH_VALUE && axisValue > -CONTROLLER_AXIS_DEATH_VALUE) {
     return 0;
   }
 
   return axisValue;
 }
 
-void onConnectedController(Controller *c)
-{
-  if (controller == nullptr)
-  {
+void onConnectedController(Controller *c) {
+  if (controller == nullptr) {
     controller = c;
   }
 }
 
-void onDisconnectedController(Controller *c)
-{
-  if (controller == c)
-  {
+void onDisconnectedController(Controller *c) {
+  if (controller == c) {
     controller = nullptr;
   }
 }
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
   Serial.printf("Firmware: %s\n", BP32.firmwareVersion());
   const uint8_t *addr = BP32.localBdAddress();
@@ -65,32 +55,27 @@ void setup()
   BP32.enableVirtualDevice(false);
 }
 
-void loop()
-{
+void loop() {
   BP32.update();
 
   speed = 0;
   speedL = 0;
   speedR = 0;
 
-  if (controller != nullptr)
-  {
+  if (controller != nullptr) {
     int32_t y = ignoreControllerAxisDeathZone(hardcapControllerAxisValue(controller->axisRY()));
     int32_t x = hardcapControllerAxisValue(controller->axisX());
 
     speed = -y * 100 / CONTROLLER_AXIS_MAX_VALUE;
 
-    if (speed > MOTOR_MIN_SPEED)
-    {
+    if (speed > MOTOR_MIN_SPEED) {
       speedL = speedR = speed;
 
-      if (x > 0)
-      {
+      if (x > 0) {
         speedR = speedR - x * 100 / CONTROLLER_AXIS_MAX_VALUE;
       }
 
-      if (x < 0)
-      {
+      if (x < 0) {
         speedL = speedL + x * 100 / CONTROLLER_AXIS_MAX_VALUE;
       }
 
@@ -98,17 +83,14 @@ void loop()
       motors.turn(speedL, speedR);
     }
 
-    if (speed < -MOTOR_MIN_SPEED)
-    {
+    if (speed < -MOTOR_MIN_SPEED) {
       speedL = speedR = speed;
 
-      if (x > 0)
-      {
+      if (x > 0) {
         speedL -= x * 100 / CONTROLLER_AXIS_MAX_VALUE;
       }
 
-      if (x < 0)
-      {
+      if (x < 0) {
         speedR += x * 100 / CONTROLLER_AXIS_MAX_VALUE;
       }
 
@@ -117,8 +99,7 @@ void loop()
     }
   }
 
-  if (!speed)
-  {
+  if (!speed) {
     motors.stop();
   }
 
