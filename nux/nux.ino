6650da43f61b4b62be1824f6d87d26a07cc9591c
lmarqs <marques.analista@gmail.com>
2023-12-19 00:05:53 -0300

    format source

--- nux/nux.ino
+++ nux/nux.ino
@@ -1,38 +1,32 @@
 #include <Arduino.h>
+
 #include <IRremote.hpp>
 
 int const MAX_SPEED = 80;
 uint8_t const PIN_IN1 = 5;
 uint8_t const PIN_IN2 = 6;
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
   IrReceiver.begin(3);
   pinMode(PIN_IN1, OUTPUT);
   pinMode(PIN_IN2, OUTPUT);
 }
 
-void loop()
-{
-  if (IrReceiver.decode())
-  {
-    if (IrReceiver.decodedIRData.command == 0x18)
-    {
+void loop() {
+  if (IrReceiver.decode()) {
+    if (IrReceiver.decodedIRData.command == 0x18) {
       analogWrite(PIN_IN1, MAX_SPEED);
       analogWrite(PIN_IN2, 0);
     }
 
-    if (IrReceiver.decodedIRData.command == 0x52)
-    {
+    if (IrReceiver.decodedIRData.command == 0x52) {
       analogWrite(PIN_IN1, 0);
       analogWrite(PIN_IN2, MAX_SPEED);
     }
 
     IrReceiver.resume();
-  }
-  else
-  {
+  } else {
     analogWrite(PIN_IN1, 0);
     analogWrite(PIN_IN2, 0);
   }
