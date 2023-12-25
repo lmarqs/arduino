36da81f23c83e42d2d0776d730e34b079024168a
lmarqs <marques.analista@gmail.com>
2023-12-24 23:01:44 -0300

    Update WEB_SERVER_HOST IP address

--- rocketspy/data/index.js
+++ rocketspy/data/index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const WEB_SERVER_HOST = location.hostname === "localhost"
-  ? "192.168.1.15"
+  ? "192.168.4.1"
   : location.host;
 
 document.addEventListener("DOMContentLoaded", () => {
