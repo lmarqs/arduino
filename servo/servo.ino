f34870fce7818beb93b92c0667f58c646cde8f17
lmarqs <marques.analista@gmail.com>
2023-12-27 12:10:56 -0300

    Update servo variable name and fix servo range

--- servo/servo.ino
+++ servo/servo.ino
@@ -1,20 +1,20 @@
 #include <Servo.h>
 
-Servo myservo;
+Servo servo;
 
 int pos = 0;
 const int min = 90;
 const int max = 90;
-void setup() { myservo.attach(9); }
+void setup() { servo.attach(9); }
 
 void loop() {
   for (pos = min; pos <= max; pos += 1) {
-    myservo.write(pos);
+    servo.write(pos);
     delay(15);
   }
 
   for (pos = max; pos >= min; pos -= 1) {
-    myservo.write(pos);
+    servo.write(pos);
     delay(15);
   }
 }
