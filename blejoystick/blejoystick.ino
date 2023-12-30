89d7ae4873fc491adea4c6110428b628565a3d26
lmarqs <marques.analista@gmail.com>
2023-12-29 21:52:11 -0300

    Refactor joystick class and add ignoreDeathZone function

--- blejoystick/blejoystick.ino
+++ blejoystick/blejoystick.ino
@@ -9,10 +9,24 @@
 
 BLECharacteristic *userInput;
 
+int16_t ignoreDeathZone(int16_t value, int16_t threshold) {
+  if (value > -threshold && value < threshold) {
+    return 0;
+  }
+
+  return value;
+}
+
 class {
+ private:
+  bool button = false;
+  int16_t x = 0;
+  int16_t y = 0;
+
  public:
-  uint8_t buffer[3] = {0, 0, 0};
-  size_t size = 3;
+  bool getButton() { return button; }
+  int16_t getX() { return x; }
+  int16_t getY() { return y; }
 
   void begin() {
     pinMode(A0, INPUT);
@@ -21,15 +35,17 @@ class {
   }
 
   void read() {
-    buffer[0] = analogRead(A0) == 0;
-    buffer[1] = map(analogRead(A1), 0, 4095, -100, 100);
-    buffer[2] = map(analogRead(A2), 0, 4095, -100, 100);
+    button = analogRead(A0) == 0;
+    x = map(analogRead(A1), 0, 4095, -2048, 2047);
+    y = map(analogRead(A2), 0, 4095, -2048, 2047);
   }
 } joystick;
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting...");
+
+  joystick.begin();
 
   BLEDevice::init(DEVICE_NAME);
   BLEServer *server = BLEDevice::createServer();
@@ -51,7 +67,19 @@ void setup() {
 
 void loop() {
   joystick.read();
-  userInput->setValue(joystick.buffer, joystick.size);
+
+  bool button = joystick.getButton();
+  int16_t x = ignoreDeathZone(joystick.getX(), 520);
+  int16_t y = ignoreDeathZone(joystick.getY(), 700);
+
+  int8_t speedL = map((2048 + min((int16_t)0, x)) / 2048 * y, -2048, 2047, -100, 100);
+  int8_t speedR = map((2048 - max((int16_t)0, x)) / 2048 * y, -2048, 2047, -100, 100);
+
+  Serial.printf("%d\t%d\t%d\t%d\t%d\n", button, x, y, speedL, speedR);
+
+  uint8_t data[] = {button, speedL, speedR};
+
+  userInput->setValue(data, 3);
   userInput->notify();
   delay(50);
 }
